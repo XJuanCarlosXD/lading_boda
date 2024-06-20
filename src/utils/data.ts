a3e67528e07a3044ece097dc746367d8c197a46e
XJuanCarlosXD <juancarlos192003@gmail.com>
2024-06-20 10:19:16 -0400

    update 8 files

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -95,4 +95,5 @@ export const data: { nombre1: string; nombre2: string; telefono: string }[] = [
 
   { nombre1: "Altagracia (mi vecina)", nombre2: "", telefono: "8099414268" },
   { nombre1: "Rolando Fernández", nombre2: "", telefono: "8096962142" },
+  { nombre1: "Ruddy Ganna", nombre2: "Amanda Ganna", telefono: "8099148612" },
 ];
